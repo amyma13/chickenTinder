848726be2195604f87110a731e7f49f355b4f101
Colin Jette <c.jette@wustl.edu>
2023-10-16 23:29:39 -0500

    PREVENT DEFAULT

--- chicken-tinder/src/login.js
+++ chicken-tinder/src/login.js
@@ -9,33 +9,35 @@ function Login() {
   const history = useHistory();
 
   const navigateToHomePage = () => {
+    
     console.log('Navigating to the homepage');
     // You can implement the navigation logic here
-    history.push('/homepage');
+    //history.push('/homepage');
   };
 
-    // async function pushAccount(){
-    //     console.log("pushed");
-    //     signInWithPopup(auth, provider)
-    //     .then((result) => {
-    //     // This gives you a Google Access Token. You can use it to access the Google API.
-    //     const credential = GoogleAuthProvider.credentialFromResult(result);
-    //     const token = credential.accessToken;
-    //     // The signed-in user info.
-    //     const user = result.user;
-    //     // IdP data available using getAdditionalUserInfo(result)
-    //     // ...
-    //     }).catch((error) => {
-    //     // Handle Errors here.
-    //     const errorCode = error.code;
-    //     const errorMessage = error.message;
-    //     // The email of the user's account used.
-    //     const email = error.customData.email;
-    //     // The AuthCredential type that was used.
-    //     const credential = GoogleAuthProvider.credentialFromError(error);
-    //     // ...
-    //     });
-    // }
+    function pushAccount(e){
+        e.preventDefault();
+        console.log("pushed");
+        // signInWithPopup(auth, provider)
+        // .then((result) => {
+        // // This gives you a Google Access Token. You can use it to access the Google API.
+        // const credential = GoogleAuthProvider.credentialFromResult(result);
+        // const token = credential.accessToken;
+        // // The signed-in user info.
+        // const user = result.user;
+        // // IdP data available using getAdditionalUserInfo(result)
+        // // ...
+        // }).catch((error) => {
+        // // Handle Errors here.
+        // const errorCode = error.code;
+        // const errorMessage = error.message;
+        // // The email of the user's account used.
+        // const email = error.customData.email;
+        // // The AuthCredential type that was used.
+        // const credential = GoogleAuthProvider.credentialFromError(error);
+        // // ...
+        // });
+    }
 
   return (
     <div className="login-container">
@@ -49,7 +51,7 @@ function Login() {
           <label>Password:</label>
           <input type="password" id="password" />
         </div>
-        <button className="login-button" onClick={navigateToHomePage}>
+        <button className="login-button" onClick={(e) => pushAccount(e)}>
           Login
         </button>
       </form>
